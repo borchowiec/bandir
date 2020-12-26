bf88e20826ff3dd934ae5ad07b67656323ce6e57
borchowiec <borchowiec.patryk@gmail.com>
2020-12-26 17:33:07 +0100

    Improved responsiveness of Register Section.

--- frontend/src/containers/Register/Register.js
+++ frontend/src/containers/Register/Register.js
@@ -4,28 +4,32 @@ import {Button, Col, Row} from "antd";
 
 import wave from '../../assets/svg/violet-wave.svg';
 import band from '../../assets/svg/band.svg';
+import bandWithoutPlate from '../../assets/svg/bandWithoutPlate.svg';
 import FormCard from "../../components/FormCard/FormCard";
 import FormInput from "../../components/FormInput/FormInput";
 
 const formInputStyle = {
   fontSize: "18px",
   marginBottom: "27px",
-  height: "52px"
 }
 
 const Register = () => (
   <div id="registerSection">
-    <Row style={{position: "absolute", width: "100%", height: "100%", zIndex: 10}}>
-      <Col span={12} style={{
+    <Row style={{width: "100%", height: "100%", zIndex: 10}}>
+      <Col sm={24} lg={12} style={{
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        textAlign: "center",
-        height: "100%"
+        textAlign: "center"
       }}>
-        <img src={band} alt="wave" style={{width: "100%", position: "relative", zIndex: 1}}/>
+        <Col xs={0} lg={24}>
+          <img src={band} alt="wave" style={{width: "100%", position: "relative", zIndex: 1}}/>
+        </Col>
+        <Col sm={24} lg={0}>
+          <img src={bandWithoutPlate} alt="wave" style={{width: "100%", position: "relative", zIndex: 1}}/>
+        </Col>
       </Col>
-      <Col span={12} style={{
+      <Col sm={24} lg={12} style={{
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
@@ -46,8 +50,8 @@ const Register = () => (
           <Button className="signUpButton">SIGN UP</Button>
         </FormCard>
       </Col>
+      <img src={wave} alt="wave" className="purpleWave" style={{width: "100%", position: "absolute", zIndex: -1}}/>
     </Row>
-    <img src={wave} alt="wave" style={{width: "100%", position: "relative", zIndex: 1}}/>
   </div>
 );
 
