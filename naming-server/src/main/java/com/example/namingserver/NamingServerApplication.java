9fd4f867a564a95364cf1f29f71424f611c74df2
borchowiec <borchowiec.patryk@gmail.com>
2020-12-29 18:30:26 +0100

    Added eureka naming server.

--- naming-server/src/main/java/com/example/namingserver/NamingServerApplication.java
+++ naming-server/src/main/java/com/example/namingserver/NamingServerApplication.java
@@ -1,15 +1,16 @@
-package com.borchowiec.test;
+package com.example.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
+@EnableEurekaServer
 @SpringBootApplication
-public class TestApplication {
+public class NamingServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestApplication.class, args);
+        SpringApplication.run(NamingServerApplication.class, args);
     }
 
 }
