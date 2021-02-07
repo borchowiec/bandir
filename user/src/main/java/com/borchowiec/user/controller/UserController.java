93d18454de21d0ad4dc100bd44aa80d822bc0d24
Patryk Borchowiec <patryk.borchowiec@jlupin.com>
2021-02-07 17:32:28 +0100

    Removed repository from user service.

--- user/src/main/java/com/borchowiec/user/controller/UserController.java
+++ user/src/main/java/com/borchowiec/user/controller/UserController.java
@@ -2,7 +2,6 @@ package com.borchowiec.user.controller;
 
 import com.borchowiec.user.model.User;
 import com.borchowiec.user.payload.CreateUserRequest;
-import com.borchowiec.user.repository.UserRepository;
 import com.borchowiec.user.service.UserService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
