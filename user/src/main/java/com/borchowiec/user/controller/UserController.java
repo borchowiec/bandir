29d7084ccd22797baa6e2f56142725cc93d3af64
Patryk Borchowiec <patryk.borchowiec@jlupin.com>
2021-02-11 19:57:50 +0100

    Added UserRepositoryClient

--- user/src/main/java/com/borchowiec/user/controller/UserController.java
+++ user/src/main/java/com/borchowiec/user/controller/UserController.java
@@ -1,30 +1,20 @@
 package com.borchowiec.user.controller;
 
-import com.borchowiec.user.model.User;
 import com.borchowiec.user.payload.CreateUserRequest;
 import com.borchowiec.user.service.UserService;
 import com.borchowiec.user.util.ValidationUtil;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
-    private final WebClient webClient;
     private final ValidationUtil validationUtil;
 
-    public UserController(UserService userService, WebClient webClient, ValidationUtil validationUtil) {
+    public UserController(UserService userService, ValidationUtil validationUtil) {
         this.userService = userService;
-        this.webClient = webClient;
         this.validationUtil = validationUtil;
-    }
-
-    @GetMapping
-    public Flux<User> findAll() {
-        return webClient.get().uri("/user-repository").retrieve().bodyToFlux(User.class);
     }
 
     @PostMapping
--- user/src/main/java/com/borchowiec/user/service/UserService.java
+++ user/src/main/java/com/borchowiec/user/service/UserService.java
@@ -1,31 +1,29 @@
 package com.borchowiec.user.service;
 
 import com.borchowiec.user.client.NotificationClient;
+import com.borchowiec.user.client.UserRepositoryClient;
 import com.borchowiec.user.event.UserCreatedEvent;
 import com.borchowiec.user.exception.AlreadyTakenException;
 import com.borchowiec.user.model.User;
 import com.borchowiec.user.model.WsMessage;
 import com.borchowiec.user.payload.CreateUserRequest;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Service
 public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final ApplicationEventPublisher publisher;
-    private final WebClient webClient;
+    private final UserRepositoryClient userRepositoryClient;
     private final NotificationClient notificationClient;
 
-    public UserService(PasswordEncoder passwordEncoder, ApplicationEventPublisher publisher, WebClient webClient,
-                       NotificationClient notificationClient) {
+    public UserService(PasswordEncoder passwordEncoder, ApplicationEventPublisher publisher,
+                       UserRepositoryClient userRepositoryClient, NotificationClient notificationClient) {
         this.passwordEncoder = passwordEncoder;
         this.publisher = publisher;
-        this.webClient = webClient;
+        this.userRepositoryClient = userRepositoryClient;
         this.notificationClient = notificationClient;
     }
 
@@ -39,11 +37,8 @@ public class UserService {
     }
 
     private Mono<Boolean> checkIfUsernameIsAlreadyTaken(CreateUserRequest request, String wsSession) {
-        return webClient
-                .get()
-                .uri("/user-repository/existsByUsername/{username}", request.getUsername())
-                .retrieve()
-                .bodyToMono(Boolean.class)
+        return userRepositoryClient
+                .existsByUsername(request.getUsername())
                 .doOnSuccess(usernameAlreadyTaken -> {
                     if (usernameAlreadyTaken) {
                         String message = String.format("Username '%s' already taken", request.getUsername());
@@ -55,11 +50,8 @@ public class UserService {
     }
 
     private Mono<Boolean> checkIfEmailIsAlreadyTaken(CreateUserRequest request, String wsSession) {
-        return webClient
-                .get()
-                .uri("/user-repository/existsByEmail/{email}", request.getEmail())
-                .retrieve()
-                .bodyToMono(Boolean.class)
+        return userRepositoryClient
+                .existsByEmail(request.getEmail())
                 .doOnSuccess(emailAlreadyTaken -> {
                     if (emailAlreadyTaken) {
                         String message = String.format("Email '%s' already taken", request.getEmail());
@@ -70,20 +62,10 @@ public class UserService {
                 });
     }
 
-    private Mono<User> saveUser(User user, String wsSession) {
-        return webClient
-                .post()
-                .uri("/user-repository")
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .bodyValue(user)
-                .retrieve()
-                .bodyToMono(User.class)
-                .doOnSuccess(entity -> publisher.publishEvent(new UserCreatedEvent(entity, wsSession)));
-    }
-
     public Mono<User> saveUser(CreateUserRequest request, String wsSession) {
         return checkIfUsernameIsAlreadyTaken(request, wsSession)
                 .then(checkIfEmailIsAlreadyTaken(request, wsSession))
-                .then(saveUser(toUser(request), wsSession));
+                .then(userRepositoryClient.save(toUser(request))
+                        .doOnSuccess(entity -> publisher.publishEvent(new UserCreatedEvent(entity, wsSession))));
     }
 }
