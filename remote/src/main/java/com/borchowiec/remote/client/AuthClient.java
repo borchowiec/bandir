271a2a068333c79501535bf474af3bde9808727b
Patryk Borchowiec <patryk.borchowiec@jlupin.com>
2021-03-07 14:51:06 +0100

    Added client's authorize method.

--- remote/src/main/java/com/borchowiec/remote/client/AuthClient.java
+++ remote/src/main/java/com/borchowiec/remote/client/AuthClient.java
@@ -3,11 +3,14 @@ package com.borchowiec.remote.client;
 import com.borchowiec.remote.model.Password;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class AuthClient {
@@ -26,5 +29,20 @@ public class AuthClient {
                 .bodyValue(Collections.singletonMap("password", password))
                 .retrieve()
                 .bodyToMono(Password.class);
+    }
+
+    public Mono<ResponseEntity> authorize(String authToken, String securityExpression) {
+        Map<String, String> body = new HashMap<>();
+        body.put("authToken", authToken);
+        body.put("securityExpression", securityExpression);
+
+        String url = "/auth/hash-password";
+        return webClient
+                .post()
+                .uri(url)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .bodyValue(body)
+                .retrieve()
+                .bodyToMono(ResponseEntity.class);
     }
 }
